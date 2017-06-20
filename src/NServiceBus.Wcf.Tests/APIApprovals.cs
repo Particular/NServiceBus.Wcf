a48b9cb0c1836d9c5c5ba07188cafb9a9c61cf42
Simon Cropp <simon.cropp@gmail.com>
2017-06-20 13:33:22 +1000

    update to new csproj

---
+++ src/NServiceBus.Wcf.Tests/APIApprovals.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using ApiApprover;
+using NServiceBus;
+using NUnit.Framework;
+
+[TestFixture]
+public class APIApprovals
+{
+    [Test]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public void Approve()
+    {
+        Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        PublicApiApprover.ApprovePublicApi(typeof(WcfEndpointConfigurationExtensions).Assembly);
+    }
+}
