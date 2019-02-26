05c180b1a3f28922620a8cdbfca33f098be53238
Brandon Ording <bording@gmail.com>
2019-02-26 15:37:44 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.Wcf.Tests/API/APIApprovals.cs
+++ src/NServiceBus.Wcf.Tests/API/APIApprovals.cs
@@ -1,38 +1,18 @@
 ﻿namespace NServiceBus.Core.Tests.API
 {
-    using System;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
-    using ApiApprover;
-    using ApprovalTests;
-    using Mono.Cecil;
-    using NUnit.Framework;
     using Features;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using PublicApiGenerator;
 
     [TestFixture]
     public class APIApprovals
     {
         [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public void ApproveWcfSupport()
         {
-            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
-            var assemblyPath = Path.GetFullPath(typeof(WcfSupport).Assembly.Location);
-            var asm = AssemblyDefinition.ReadAssembly(assemblyPath);
-            var publicApi = Filter(PublicApiGenerator.CreatePublicApiForAssembly(asm));
-            Approvals.Verify(publicApi);
-        }
-
-        string Filter(string text)
-        {
-            return string.Join(Environment.NewLine, text.Split(new[]
-            {
-                Environment.NewLine
-            }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
-                .Where(l => !string.IsNullOrWhiteSpace(l))
-                );
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(WcfSupport).Assembly);
+            Approver.Verify(publicApi);
         }
     }
 }
